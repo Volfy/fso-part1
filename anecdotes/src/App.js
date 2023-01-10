5401b14c7d7ada0aa22640f68d3099ebfa7d5a90
sasha v <volfym79@gmail.com>
2023-01-10 19:42:20 +0100

    exercise 1.12

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -13,9 +13,14 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
 
+  const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+
   return (
     <div>
-      {anecdotes[selected]}
+      <p>
+        {anecdotes[selected]}
+      </p>
+      <button onClick={handleClick}>New Anecdote</button>
     </div>
   )
 }
